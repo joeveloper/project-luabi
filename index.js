1f3d25beb08a8affc5b1948eda611f230273cfec
joeveloper <bebosir@gmail.com>
2019-12-18 11:50:27 +0100

    preparing for productiongit status

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const mongoURI = require('./config').mongoURI
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const mongoURI = require("./config").mongoURI;
 
 //middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //database connection
-mongoose.connect(mongoURI, {useNewUrlParser: true,
-useCreateIndex: true, useUnifiedTopology: true  }, ()=>{
- console.log('Connected to the database');
-})
+mongoose.connect(
+  mongoURI,
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to the database");
+  }
+);
 
-mongoose.set('useFindAndModify', false) //used because of deprecation issue
+mongoose.set("useFindAndModify", false); //used because of deprecation issue
 
 //routes
-app.use('/api', require('./routes/post'))
+app.use("/api", require("./routes/post"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("my-app/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "my-app", "build", "index.html"));
+  });
+}
 
-const PORT = 5000
-app.listen(PORT, ()=>console.log(`The server is running on port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
